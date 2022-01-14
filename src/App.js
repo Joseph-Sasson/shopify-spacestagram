93f0e14fe26030dd0a865208ac45022578aa2c17
Joseph <josephsasson1@gmail.com>
2022-01-14 12:43:22 -0500

    add fetch for nasa api

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState()
+
+  useEffect(()=>{
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}`)
+    .then(res=>res.json())
+    .then(setUser)
+  },[])
+
   return (
     <div className="App">
-      Spacestagram
+      
     </div>
   );
 }
